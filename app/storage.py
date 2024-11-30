95c9aebc88b66e8175bacfb133aa68eb23911d26
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-11-30 14:34:27 +0300

    rewrite storage class

--- app/storage.py
+++ app/storage.py
@@ -1,54 +1,65 @@
 import json
+from datetime import datetime
 
-from app.model import BookStatus, Book
+from app.model.enums import Category, TaskPriority, TaskStatus
+from app.model.task import Task
+from app.utils import sort_tasks
 
 
-
-class Storage:
+class TaskStorage:
     """
-        Класс Storage предназначен для управления хранением и сохранением книг в формате JSON.
+    Класс Storage предназначен для управления хранением и сохранением задач в формате JSON.
 
-        Args:
-            self.storage_file (str): имя файла для хранения книг
+    Args:
+        self.storage_file (str): имя файла для хранения задач
     """
-    def __init__(self) -> None:
-        self.storage_file = "books.json"
 
-    def save_books(self, books) -> None:
-        """
-        Метод для сохранения списка книг в формате JSON в указанный файл или созднание этого файла при
-        его отсутствие
-        """
-        with open(self.storage_file, 'w', encoding='utf-8') as file:
-            json.dump([book.to_dict() for book in books], file, ensure_ascii=False, indent=4)
-        print("Изменения сохранены в файл.\n")
+    def __init__(self, storage_file: str = "tasks.json") -> None:
+        self.storage_file = storage_file
 
-
-    def load_books(self) -> list[Book | None]:
+    def save_task_json(self, tasks) -> None:
         """
-        Метод для загрузки книг из файла JSON и преобразования их в список моделей Book
+        Метод для сохранения списка задач в формате JSON в указанный файл или созднание этого файла,
+        при его отсутствие
+        """
+        with open(self.storage_file, "w", encoding="utf-8") as file:
+            json.dump([task.to_dict() for task in tasks], file, ensure_ascii=False, indent=4)
+        print("\nИзменения сохранены в файл.\n")
+
+    def load_task_json(self) -> list[Task | None]:
+        """
+        Метод для загрузки задач из файла JSON и преобразования их в список моделей Task
         """
         try:
             with open(self.storage_file, "r", encoding="utf-8") as file:
-                books_data = json.load(file)
+                tasks_data = json.load(file)
 
                 # Преобразование словаря в модель Book и создание списка книг
                 # Возможно выглядит сложно потому, что обязательно нужно привести статус в Enum
-                unsorted_books: list[Book | None] = [
-                    Book(
-                        **{**data, "status": BookStatus.from_value(data["status"])}
+                unsorted_tasks: list[Task | None] = [
+                    Task(
+                        **{
+                            **data,
+                            "category": Category.from_value(data["category"]),
+                            "priority": TaskPriority.from_value(data["priority"]),
+                            "status": TaskStatus.from_value(data["status"]),
+                            "due_date": (
+                                datetime.strptime(data["due_date"], "%Y-%m-%d").date()
+                                if "due_date" in data
+                                else None
+                            ),
+                        }
                     )
-                    for data in books_data
+                    for data in tasks_data
                 ]
-                # Сортировка книг по id, на случай, если книги в файле были переставлены,
-                # так как будет генерироваться неправильный id, он генерируется: books[-1] + 1
-                return sorted(unsorted_books, key=lambda book: book.id)
+
+                return sort_tasks(tasks=unsorted_tasks)
 
         except FileNotFoundError:
             return []
-        except json.JSONDecodeError as exc:
-            print(f"Ошибка при загрузке книг из файла: {exc}")
+        except json.JSONDecodeError:
+            print("Пока ваш файл пуст\n")
             return []
         except KeyError as exc:
-            print(f"Ошибка преобразования статуса книги: {exc}")
+            print(f"Ошибка преобразования категории/приоритета/статуса/даты задачи: {exc}")
             return []
