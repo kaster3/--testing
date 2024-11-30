fe693c466b5500632930824994cdfda42afb55ba
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-11-30 15:05:34 +0300

    delete comment

--- app/model/task.py
+++ app/model/task.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from .enums import Category, TaskPriority, TaskStatus
 
 
-# @id_generator
+
 class Task:
     """Основная бизнес сущность с которой будут выполняться все CRUD операции"""
 
@@ -52,3 +52,4 @@ class Task:
             "priority": self.priority.value,
             "status": self.status.value,
         }
+
