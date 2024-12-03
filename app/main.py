958e43e140b56ff8f3d932804678110c48010e3d
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-12-03 22:47:50 +0300

    replace hardcode

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from messages.main.consts import MAIN_EXIT, WRONG_INPUT
+from messages.main.message_formatter import main_exception
 from repository import TaskRepository
 from service_layer import TaskService
 from service_layer.service_helper import ServiceHelper
@@ -14,37 +16,24 @@ def main() -> None:
     service_helper = ServiceHelper()
     service = TaskService(repository=repository, service_helper=service_helper)
 
+    choices = {
+        "1": service.show_tasks,
+        "2": service.add_task,
+        "3": service.update_task,
+        "4": service.delete_task,
+        "5": service.search_by_key_word,
+        "6": lambda: print(MAIN_EXIT),
+    }
+
     while True:
         choice = service.get_choice()
-
-        if choice == "1":
-            service.show_tasks()
-
-        elif choice == "2":
-            service.add_task()
-
-        elif choice == "3":
-            service.update_task()
-
-        elif choice == "4":
-            service.delete_task()
-
-        elif choice == "5":
-            service.search_by_key_word()
-
-        elif choice == "6":
-            print("Выход из программы.")
+        choices.get(choice, lambda: print(WRONG_INPUT))()
+        if choice == "6":
             break
-
-        else:
-            print("Неверный выбор. Выбери число от 1 до 6 включительно.\n")
 
 
 if __name__ == "__main__":
     try:
         main()
     except Exception as exc:
-        print(
-            f"Кажется это операция не работает,"
-            f" свяжитесь с разработчиком.\n{exc.__class__.__name__} -> {exc}"
-        )
+        print(main_exception(exc=exc))
