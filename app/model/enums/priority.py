a353d1be39b8f4194fc2f132b5f5032f63a5802a
Fedorov Semen <FedorovSemen28@yandex.ru>
2024-12-04 02:39:03 +0300

    delete from_value

--- app/model/enums/priority.py
+++ app/model/enums/priority.py
@@ -10,13 +10,3 @@ class TaskPriority(Enum):
     HIGH = "Высокий"
     MEDIUM = "Средний"
     LOW = "Низкий"
-
-    @staticmethod
-    def from_value(value: str) -> "TaskPriority":
-        """
-        Метод для получения приоритета задачи по его значению, для того чтобы
-        json корректно работал с Enum
-        """
-        for status in TaskPriority:
-            if status.value == value:
-                return status
